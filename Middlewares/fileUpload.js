b94981a5bf92512e98a711fc19701e10b4fb675c
pratik javale <134946535+javalepratik7@users.noreply.github.com>
2025-05-28 10:14:40 +0530

    Update fileUpload.js
    change path

--- Middlewares/fileUpload.js
+++ Middlewares/fileUpload.js
@@ -5,7 +5,7 @@ const path = require("path");
 const storage=multer.diskStorage({
     destination: (req,file,cb)=> {
         console.log("commng to multer",file);
-            cb(null,"./images")
+            cb(null,"./Services")
         },
         filename: (req,file,cb) =>{
             cb(null,file.originalname)
